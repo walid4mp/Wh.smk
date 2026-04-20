6fd786b8ed7e5b4c0d53f71c43c71789d510eb99
walid4mp <walid4mco@gmail.com>
2026-04-20 10:39:37 +0100

    switch edge to nodejs

--- app/api/get-next-completion-stream-promise/route.ts
+++ app/api/get-next-completion-stream-promise/route.ts
@@ -89,5 +89,5 @@ export async function POST(req: Request) {
   return new Response(res.toReadableStream());
 }
 
-export const runtime = "edge";
+export const runtime = "nodejs";
 export const maxDuration = 300;
